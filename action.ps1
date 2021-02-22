24162d01eeeff674a78685bcb29ed3f7151124ab
Andre Wojciechowski <andre.wojciechowski@microsoft.com>
2021-02-22 07:35:45 +0100

    force

--- action.ps1
+++ action.ps1
@@ -2,4 +2,4 @@ Write-host "from pwsh with love"
 Get-ChildItem -Recurse
 Get-ChildItem env:* | fl
 Get-Module -ListAvailable
-Install-Module Az -Scope CurrentUser
+Install-Module Az -Scope CurrentUser -Force
